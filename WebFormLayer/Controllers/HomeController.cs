a2494d4edb8835485d59e0fe632a80f2da658b36
Agusricc78 <122297049+Agusricc78@users.noreply.github.com>
2024-11-30 19:07:25 -0300

    c

--- WebFormLayer/Controllers/HomeController.cs
+++ WebFormLayer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,23 +14,21 @@ namespace WebFormLayer.Controllers
             return View();
         }
 
-        public ActionResult HomTest() 
+        public ActionResult Carrito()
         {
             return View();
         }
 
-
-
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
