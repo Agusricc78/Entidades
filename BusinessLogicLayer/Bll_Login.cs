17ff6c9d0b904d47fd372682cae2c4d1b4a15668
Agusricc78 <122297049+Agusricc78@users.noreply.github.com>
2024-11-30 20:06:06 -0300

    commit Login

---
+++ BusinessLogicLayer/Bll_Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Entities;
+
+namespace BusinessLogicLayer
+{
+    public class LoginService
+    {
+        
+        private readonly DataAccess _dataAccess;
+
+      
+        public LoginService()
+        {
+            _dataAccess = new DataAccess(); 
+        }
+
+ 
+        public Usuario ValidateLogin(string username, string password)
+        {
+            // El nombre del stored procedure para obtener el usuario por nombre de usuario y contraseña
+            string sp = "sp_GetUserByUsernameAndPassword";
+
+            
+            var user = new Usuario
+            {
+                Username = username,
+                Password = password
+            };
+
+           
+            var usuario = _dataAccess.Get<Usuario>(sp, user);
+
+            return usuario;
+        }
+    }
+}
---
+++ WebFormLayer/Controllers/Login.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogicLayer;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebFormLayer.Controllers
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verifica si el usuario ya está autenticado
+            if (Session["Usuario"] != null)
+            {
+                Response.Redirect("Home.aspx");
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = username.Text;
+            string password = password.Text;
+
+            // Utilizamos la instancia de LoginService para validar el login
+            var loginService = new LoginService();
+            Usuario user = loginService.ValidateLogin(username, password);
+
+            if (user != null)
+            {
+                // Guardar sesión y redirigir
+                Session["Usuario"] = user;
+                Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                // Mostrar mensaje de error
+                Response.Write("<script>alert('Usuario o contraseña incorrectos');</script>");
+            }
+        }
+    }
+}
