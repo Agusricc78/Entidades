69ff831ab823a1a59cd61da3b18a06d64be0ee22
Agusricc78 <122297049+Agusricc78@users.noreply.github.com>
2024-12-05 17:03:11 -0300

    Commit

---
+++ WebFormLayer/Controllers/PagoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFormLayer.Controllers
+{
+    public class PagoController : Controller
+    {
+        // GET: Pago
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PagoController()
+        {
+            // Configura las credenciales de MercadoPago
+            SDK.Initialize("TU_CLIENT_ID", "TU_CLIENT_SECRET");
+        }
+
+
+
+
+    }
+}
